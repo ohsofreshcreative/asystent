e340621cc97d6cf40746cfa4c611f549b58c3054
Arek <kontakt@ohsofresh.pl>
2025-11-18 17:27:48 +0100

    Automatyczny deploy

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-	<div class="__wrap bg-amber-300 relative">
+	<div class="__wrap bg-gradient relative">
 		<div class="__wrapper c-main relative z-10">
 			<div class="__widgets grid gap-1 md:gap-6 py-36">
 				@for ($i = 1; $i <= 4; $i++)
@@ -14,7 +14,7 @@
 	<div class="c-main flex flex-col md:flex-row justify-between gap-6 py-10 footer-bottom">
 		<p class="">Copyright ©2025 <?php echo get_bloginfo('name'); ?>. All Rights Reserved</p>
 		<p class="flex gap-2">Designed &amp; Developed by
-			<a target="_blank" href="https://www.ohsofresh.pl" title="OhSoFresh"><img class="oh" src="/wp-content/themes/tlc/resources/images/ohsofresh.svg"></a>
+			<a target="_blank" href="https://www.ohsofresh.pl" title="OhSoFresh"><img class="oh" src="/wp-content/themes/asystent/resources/images/ohsofresh.svg"></a>
 		</p>
 	</div>
 	</div>
