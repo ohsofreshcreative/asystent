2d69fcb5c8b102f8b49f227f8b080f89e74c7172
Arek <kontakt@ohsofresh.pl>
2025-12-05 09:46:18 +0100

    Automatyczny deploy

--- resources/views/blocks/content.blade.php
+++ resources/views/blocks/content.blade.php
@@ -6,7 +6,7 @@ $sectionClass .= $nomt ? ' !mt-0' : '';
 $sectionClass .= $gap ? ' wider-gap' : '';
 
 if (!empty($background) && $background !== 'none') {
-    $sectionClass .= ' ' . $background;
+$sectionClass .= ' ' . $background;
 }
 @endphp
 
@@ -15,27 +15,20 @@ if (!empty($background) && $background !== 'none') {
 <section data-gsap-anim="section" @if(!empty($section_id)) id="{{ $section_id }}" @endif class="b-content relative -smt {{ $sectionClass }} {{ $section_class }}">
 
 	<div class="__wrapper c-main relative">
-		<div class="__col grid grid-cols-1 lg:grid-cols-2 items-center gap-6 md:gap-16">
-			@if (!empty($g_content['image']))
-			<div data-gsap-element="image" class="__img h-full order1">
-				<img class="object-cover w-full h-full radius-img" src="{{ $g_content['image']['url'] }}" alt="{{ $g_content['image']['alt'] ?? '' }}">
+
+		<div class="__content order2">
+			<h4 data-gsap-element="header" class="m-header">{{ $g_content['title'] }}</h4>
+
+			<div data-gsap-element="txt" class="__txt mt-2">
+				{!! $g_content['txt'] !!}
 			</div>
+
+			@if (!empty($g_content['button']))
+			<a data-gsap-element="btn" class="main-btn m-btn" href="{{ $g_content['button']['url'] }}">{{ $g_content['button']['title'] }}</a>
 			@endif
 
-			<div class="__content order2">
-				<h4 data-gsap-element="header" class="m-header">{{ $g_content['title'] }}</h4>
-
-				<div data-gsap-element="txt" class="__txt mt-2">
-					{!! $g_content['txt'] !!}
-				</div>
-
-				@if (!empty($g_content['button']))
-				<a data-gsap-element="btn" class="main-btn m-btn" href="{{ $g_content['button']['url'] }}">{{ $g_content['button']['title'] }}</a>
-				@endif
-
-			</div>
-
 		</div>
-		</div>
+
+	</div>
 
 </section>
